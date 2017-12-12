70b01563d900a9ca4a158fa2476e5c8619b0cf07
Takamitsu Iwai <pornoromen0101@gmail.com>
2017-12-12 15:08:03 +0900

    temp

--- loss_visualize.py
+++ loss_visualize.py
@@ -1,1 +1,13 @@
+from pathlib import Path
 import matplotlib.pyplot as plt
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    path = Path("cv/STFTCNN/2017_12_12_01_45_32")
+    history = pd.read_csv(path/"fold_0_log.csv")
+    plt.plot(history.epoch, history.acc, label="train_acc")
+    plt.plot(history.epoch, history.val_acc, label="val_acc")
+    plt.legend()
+    plt.savefig(path/"visualize_loss.pdf")

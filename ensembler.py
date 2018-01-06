d3f38749d1d043222148af4ade1fc4518b0b97ad
Takamitsu Iwai <pornoromen0101@gmail.com>
2018-01-06 18:24:38 +0900

    Conv1d 5fold CV * 3 (seed 2017 (pseudo label + not) + 3017 + 4017) + STFTCNN 5fold CV * 3 (undersampling * 2 (seed 2017 + 3017) + oversampling (2017_12_28_00_58_05.csv)) (public LB 0.87 rank max)

--- ensembler.py
+++ ensembler.py
@@ -30,6 +30,11 @@ def ensembler(sub_list, sub_name):
 if __name__ == "__main__":
 
     sub_list = ["sub/VGG1Dv2/2018_01_06_17_40_23",
-                "sub/STFTCNN/2018_01_02_13_39_20"]
+                "sub/VGG1Dv2/2018_01_01_20_16_18",
+                "sub/VGG1Dv2/2018_01_01_23_45_31",
+                "sub/VGG1Dv2/2018_01_02_02_27_55",
+                "sub/STFTCNN/2017_12_28_00_57_05",
+                "sub/STFTCNN/2018_01_03_12_53_40",
+                "sub/STFTCNN/2017_12_28_00_57_05"]
     sub_name = utils.now()
     ensembler(sub_list, sub_name)
